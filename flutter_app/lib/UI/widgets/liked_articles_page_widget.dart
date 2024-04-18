b6b09df6f1874fab3e67698d1ae8d556098dc5ef
ArsPro1323 <67059479+ArsPro13@users.noreply.github.com>
2024-04-18 21:10:42 +0300

    Update liked articles page and APK

--- flutter_app/lib/UI/widgets/liked_articles_page_widget.dart
+++ flutter_app/lib/UI/widgets/liked_articles_page_widget.dart
@@ -11,10 +11,12 @@ class LikedArticlesPageWidget extends ConsumerStatefulWidget {
   const LikedArticlesPageWidget({super.key});
 
   @override
-  ConsumerState<LikedArticlesPageWidget> createState() => LikedArticlesPageWidgetState();
+  ConsumerState<LikedArticlesPageWidget> createState() =>
+      LikedArticlesPageWidgetState();
 }
 
-class LikedArticlesPageWidgetState extends ConsumerState<LikedArticlesPageWidget> {
+class LikedArticlesPageWidgetState
+    extends ConsumerState<LikedArticlesPageWidget> {
 
   int currentlyLoaded = 5;
 
@@ -32,6 +34,7 @@ class LikedArticlesPageWidgetState extends ConsumerState<LikedArticlesPageWidget
   Widget build(BuildContext context) {
     List<ArticleData> articlesCollection =
         ref.watch(likedArticlesProvider).articles;
+    currentlyLoaded = min(currentlyLoaded, articlesCollection.length);
     return Center(
       child: NotificationListener<ScrollNotification>(
         onNotification: (notification) {
@@ -41,32 +44,37 @@ class LikedArticlesPageWidgetState extends ConsumerState<LikedArticlesPageWidget
           }
           return false;
         },
-        child: CustomScrollView(
-          slivers: [
-            SliverPadding(
-              padding: const EdgeInsets.only(top: 50),
-              sliver: SliverList.builder(
-                itemCount: currentlyLoaded + 1,
-                itemBuilder: (context, index) {
-                  if (index < currentlyLoaded) {
-                    return ArticleWidget(
-                      article: articlesCollection[index],
-                      height: 300,
-                      width: 1000,
-                      id: index,
-                    );
-                  } else if (index < articlesCollection.length) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
+        child: articlesCollection.length > 0
+            ? CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.only(top: 50),
+                    sliver: SliverList.builder(
+                      itemCount: min(
+                          currentlyLoaded + 1, articlesCollection.length + 1),
+                      itemBuilder: (context, index) {
+                        if (index < currentlyLoaded) {
+                          return ArticleWidget(
+                            article: articlesCollection[index],
+                            height: 300,
+                            width: 1000,
+                            id: index,
+                          );
+                        } else if (index < articlesCollection.length) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              )
+            : const Center(
+                child: Text('Like articles to display them on this screen'),
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
